b967e42d65b2cdb9deec2dfad4b517a61697cdf2
isaui <isadestroyed17@gmail.com>
2025-07-01 15:38:11 +0700

    update

--- utils/git_checkout_utils.go
+++ utils/git_checkout_utils.go
@@ -2,10 +2,9 @@ package utils
 
 import "fmt"
 
-// Helper to get the appropriate git checkout command
 func getCheckoutCommand(commitSHA string) string {
     if commitSHA == "" {
         return ""
     }
-    return fmt.Sprintf("&& echo 'Checking out commit %s...' && git fetch origin %s && git checkout %s && echo 'Commit checkout completed'", commitSHA, commitSHA, commitSHA)
+    return fmt.Sprintf("&& cd /workspace && echo 'Checking out commit %s...' && git fetch origin %s && git checkout %s && echo 'Commit checkout completed'", commitSHA, commitSHA, commitSHA)
 }
